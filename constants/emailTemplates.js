4df4494d5a07d2ac56bc06b7cc174dda399f772f
Rohit Patil <rohitvpatil0810@gmail.com>
2023-11-01 14:28:41 +0530

    📤 Completed Send Email Notifications Functionality

---
+++ constants/emailTemplates.js
@@ -0,0 +1,8 @@
+const emailTemplates = {
+  thankYouEmailTemplate:
+    "<p>Hi {{name}},</p><p><br /></p><p>Thank you for your message via my portfolio site! 🎉 I'll be in touch shortly to discuss your inquiry. 😊</p><p><br /></p><p>Best regards,</p><p>Rohit Patil</p><p><br /></p><p><br /></p><p><br /></p><p><br /></p><p>&nbsp;</p>",
+  notificationEmailTemplate:
+    "<p>Hello,</p><p><br /></p><p>You've got mail! 📬</p><p><br /></p><p>Sender's Name: {{name}}</p><p>Sender's Email: {{email}}</p><p>Message: {{message}}</p><p><br /></p><p>Check it out when you get a chance. 😊</p><p><br /></p><p>Best regards,</p><p>Rohit Portfolio Email Server</p><p><br /></p><p><br /></p><p><br /></p><p><br /></p><p><br /></p><p><br /></p><p><br /></p><p>&nbsp;</p>",
+};
+
+module.exports = emailTemplates;
--- controllers/email.controller.js
+++ controllers/email.controller.js
@@ -1,1 +1,65 @@
-module.exports.sendNotifications = async (req, res) => {};
+var validator = require("validator");
+const Handlebars = require("handlebars");
+const emailTemplates = require("../constants/emailTemplates");
+const { sendEmail } = require("../utility/sendEmail");
+
+module.exports.sendNotifications = async (req, res) => {
+  const { name, email, message } = req.body;
+  try {
+    // Error Handling
+    if (!name || !email || !message) {
+      res.status(400).json({
+        success: false,
+        error: "Please enter all details.",
+      });
+    }
+    if (!validator.isEmail(email)) {
+      res.status(400).json({
+        success: false,
+        error: "Please enter valid email.",
+      });
+    }
+
+    // Notification Email
+    let notificationEmailCompiler = Handlebars.compile(
+      emailTemplates.notificationEmailTemplate
+    );
+    let notificationEmail = notificationEmailCompiler({ name, email, message });
+    let notificationEmailData = {
+      receiver: process.env.MY_EMAIL,
+      subject: "New Message Alert! 💌",
+    };
+
+    let notificationEmailResult = await sendEmail(
+      notificationEmailData,
+      notificationEmail
+    );
+
+    // Thank You Email
+    let thankYouEmailCompiler = Handlebars.compile(
+      emailTemplates.thankYouEmailTemplate
+    );
+
+    let thankYouEmail = thankYouEmailCompiler({ name });
+    let thankYouEmailData = {
+      receiver: email,
+      subject: "🙏 Thank You for Reaching Out!",
+    };
+
+    let thankYouEmailResult = await sendEmail(thankYouEmailData, thankYouEmail);
+
+    // Result Response
+    res.status(200).json({
+      success: true,
+      msg: "Thank you for contacting",
+      notificationEmailResult,
+      thankYouEmailResult,
+    });
+  } catch (error) {
+    res.status(404).json({
+      success: false,
+      error: "Something went wrong!",
+      errorMessage: error,
+    });
+  }
+};
